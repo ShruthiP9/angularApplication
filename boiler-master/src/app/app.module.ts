361da4e8644e8202ce20dc847c6e3d619611bbcf
Shruthi P1 <SP00558091@techmahindra.com>
2019-07-24 18:54:07 +0530

    login page

--- boiler-master/src/app/app.module.ts
+++ boiler-master/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { LoginService } from './services/login.service';
 
 
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+
 import { NgModule, Component } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
@@ -10,6 +11,11 @@ import { Router } from '@angular/router';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { HomeComponent } from './Components/home/home.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { HelpComponent } from './Components/help/help.component';
+import { LoginComponent } from './Components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 
@@ -21,19 +27,27 @@ import { HomeComponent } from './Components/home/home.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
+    AboutUsComponent,
+    HelpComponent,
+    LoginComponent,
    
   ],
   // modules to be used
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
+    
+   
      RouterModule.forRoot([
-       {path:'',component:HeaderComponent},
+       {path:'',component:LoginComponent},
        {path:'home',component:HomeComponent},
+       {path:'about-us',component:AboutUsComponent},
+       {path:'help',component:HelpComponent}
     ])
   ],
-  providers: [RouterModule],
+  providers: [RouterModule,LoginService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ boiler-master/src/app/services/login.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {Http,Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+import {Observable} from 'rxjs/observable';
+
+@Injectable()
+export class LoginService {
+
+  tabledata=[];
+  constructor(private http:Http ){
+
+   }
+   
+   getPosts():Observable <any>{
+     return this.http.get('assets/inputs.json').map((response:Response)=>response.json());
+   }
+
+}
